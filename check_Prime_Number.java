7f756dd8b648a8b0d30f2ebe8bf7b7aa3d36553c
NikhilRPandey1 <51952165+NikhilRPandey1@users.noreply.github.com>
2021-08-17 21:33:02 +0530

    Update and rename check_Prime_Number to check_Prime_Number.java

--- check_Prime_Number.java
+++ check_Prime_Number.java
@@ -1,8 +1,10 @@
 import java.util.*;
 class check_Prime_Number{
   public static void main(String[] args){
+    // function call
   check_prime_Number();
   }
+  //function to check prime number
   private static void check_prime_Number(){
     Scanner scan = new Scanner(System.in);
     System.out.println("Enter number to check prime or not ");
